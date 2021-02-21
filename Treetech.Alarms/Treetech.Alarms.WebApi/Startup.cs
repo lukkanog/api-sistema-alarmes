eea4c1e8c78d3be8bab9a589871f3085076a4edc
lukkanog <lukanog@hotmail.com>
2021-02-21 17:23:58 -0300

    json, swagger and cors settings

--- Treetech.Alarms/Treetech.Alarms.WebApi/Startup.cs
+++ Treetech.Alarms/Treetech.Alarms.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,32 @@ namespace Treetech.Alarms.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // add controller with JSON handling settings 
+            services.AddControllers()
+                .AddNewtonsoftJson(options => {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                });
+
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Alarmes API",
+                    Description = "API de monitoramento de alarmes relacionados a equipamentos.",
+                });
+            });
+
+            //CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +61,18 @@ namespace Treetech.Alarms.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            //Use Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Teste Treetech Web API");
+            });
+
+            //Utilização do CORS
+            app.UseCors("CorsPolicy");
+
 
             app.UseHttpsRedirection();
 
