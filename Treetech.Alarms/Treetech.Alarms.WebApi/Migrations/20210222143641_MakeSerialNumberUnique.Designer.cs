e317689e539550a0cb957951b8cb7c2df4feb5d6
lukkanog <lukanog@hotmail.com>
2021-02-22 11:38:21 -0300

    db migration - create equipamento index

--- Treetech.Alarms/Treetech.Alarms.WebApi/Migrations/20210222143641_MakeSerialNumberUnique.Designer.cs
+++ Treetech.Alarms/Treetech.Alarms.WebApi/Migrations/20210222143641_MakeSerialNumberUnique.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Treetech.Alarms.WebApi.Contexts;
 
 namespace Treetech.Alarms.WebApi.Migrations
 {
     [DbContext(typeof(AlarmsContext))]
-    partial class AlarmsContextModelSnapshot : ModelSnapshot
+    [Migration("20210222143641_MakeSerialNumberUnique")]
+    partial class MakeSerialNumberUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
