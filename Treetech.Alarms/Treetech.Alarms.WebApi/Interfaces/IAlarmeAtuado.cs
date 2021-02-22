063c1dbc94e7620bcf0fa07a8254a806d7a17975
lukkanog <lukanog@hotmail.com>
2021-02-22 15:06:25 -0300

    alarme atuado repository

---
+++ Treetech.Alarms/Treetech.Alarms.WebApi/Interfaces/IAlarmeAtuado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Treetech.Alarms.WebApi.Models;
+
+namespace Treetech.Alarms.WebApi.Interfaces
+{
+    public interface IAlarmeAtuadoRepository
+    {
+        /// <summary>
+        /// Cadastra um novo alarme atuado no banco de dados.
+        /// </summary>
+        /// <param name="alarmeAtuado">AlarmeAtuado a ser cadastrado.</param>
+        /// <returns>AlarmeAtuado cadastrado.</returns>
+        public Task<AlarmeAtuado> Cadastrar(AlarmeAtuado alarmeAtuado);
+
+        /// <summary>
+        /// Lista todos os alarmes atuados presentes no banco de dados.
+        /// </summary>
+        /// <returns>Lista de alarmeAtuados.</returns>
+        public Task<List<AlarmeAtuado>> Listar();
+
+        /// <summary>
+        /// Altera o status do alarme atuado. Se ativo, será desativado e vice-versa.
+        /// </summary>
+        /// <param name="idAlarme">id do alarme a ser modificado.</param>
+        /// <returns>Alarme atuado com status alterado.</returns>
+        public Task<AlarmeAtuado> AlterarStatus(int idAlarmeAtuado);
+
+
+    }
+}
---
+++ Treetech.Alarms/Treetech.Alarms.WebApi/Repositories/AlarmeAtuadoRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Treetech.Alarms.WebApi.Contexts;
+using Treetech.Alarms.WebApi.Interfaces;
+using Treetech.Alarms.WebApi.Models;
+
+namespace Treetech.Alarms.WebApi.Repositories
+{
+    public class AlarmeAtuadoRepository : IAlarmeAtuadoRepository
+    {
+        private AlarmsContext context { get; set; }
+
+        public AlarmeAtuadoRepository()
+        {
+            context = new AlarmsContext();
+        }
+
+        //to create in memory repository (for test purporses)
+        public AlarmeAtuadoRepository(AlarmsContext alarmsContext)
+        {
+            context = alarmsContext;
+        }
+
+
+        public async Task<AlarmeAtuado> AlterarStatus(int idAlarmeAtuado)
+        {
+            var alarmeAtuado = context.AlarmesAtuados.Find(idAlarmeAtuado);
+
+            if (alarmeAtuado == null)
+                throw new Exception("Registro de alarme atuado não encontrado.");
+
+            alarmeAtuado.Ativo = !alarmeAtuado.Ativo;
+
+            context.Update(alarmeAtuado);
+            await context.SaveChangesAsync();
+
+            return alarmeAtuado;
+        }
+
+        public async Task<AlarmeAtuado> Cadastrar(AlarmeAtuado alarmeAtuado)
+        {
+            context.AlarmesAtuados.Add(alarmeAtuado);
+            await context.SaveChangesAsync();
+
+            return alarmeAtuado;
+        }
+
+        public async Task<List<AlarmeAtuado>> Listar()
+        {
+            var list = context.AlarmesAtuados
+                .Include(x => x.Alarme)
+                .ThenInclude(y => y.Equipamento)
+                .ToList();
+
+            return list;
+        }
+    }
+}
